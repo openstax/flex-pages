270f81106fcfac5c69358e71a6ff63ffa14a9935
tom <tom.g.woodward@gmail.com>
2025-03-28 12:14:48 -0400

    remove flags

--- script/build.bash
+++ script/build.bash
@@ -8,13 +8,6 @@ cd "$project_dir";
 
 tsc_args=(--noEmit false --declaration)
 
-if yarn -s ts-utils has-flag watch "$@"; then
-  tsc_args+=(--watch)
-fi
-if yarn -s ts-utils has-flag clean "$@"; then
-  rm -rf "$project_dir"/dist
-fi
-
 mkdir -p dist
 
 yarn -s tsc --project tsconfig.without-specs.esm.json "${tsc_args[@]}"
