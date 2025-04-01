61236f294abcfeda413d1f56350a9dbeebba3e04
tom <tom.g.woodward@gmail.com>
2025-04-01 12:21:36 -0400

    wip types

--- src/blocks/ContentBlock.tsx
+++ src/blocks/ContentBlock.tsx
@@ -1,52 +1,32 @@
-import { CTABlock, CTABlockConfig } from './CTABlock';
-import { CardsBlock, CardsBlockConfig } from './CardsBlock';
-import { DividerBlock, DividerBlockConfig } from './DividerBlock';
-import { HTMLBlock, HTMLBlockConfig } from './HTMLBlock';
-import { HeroBlock, HeroBlockConfig } from './HeroBlock';
-import { LinksBlock, LinksBlockConfig } from './LinksBlock';
-import { QuoteBlock, QuoteBlockConfig } from './QuoteBlock';
-import { RichTextBlock, RichTextBlockConfig } from './RichTextBlock';
-import { SectionBlock, SectionBlockConfig } from './SectionBlock';
+import React from 'react';
 
-export type ContentBlockConfig =
-    LinksBlockConfig |
-    CTABlockConfig |
-    HeroBlockConfig |
-    HTMLBlockConfig |
-    DividerBlockConfig |
-    SectionBlockConfig |
-    RichTextBlockConfig |
-    QuoteBlockConfig |
-    CardsBlockConfig;
+export type ContentBlockConfig = {type: string; id: string;};
+type BlockData<D> = D extends ContentBlockConfig ? D : never;
+type BlockComponents<D> = D extends ContentBlockConfig ? {
+  [K in D['type']]: React.ComponentType<{ data: Extract<D, { type: K }> }>;
+} : never;
+type BlockComponent = React.ComponentType<{data: ContentBlockConfig}>
+type BlockComponentMap = Record<string, BlockComponent>;
+export const BlockContext = React.createContext<BlockComponentMap>({});
+
+export function ContentBlockRoot<D>({data, blocks}: {
+  data: BlockData<D>[];
+  blocks: BlockComponents<D>;
+}) {
+  return <BlockContext.Provider value={blocks as any}>
+    <ContentBlocks data={data} />
+  </BlockContext.Provider>
+};
 
 export function ContentBlocks({data}: {data: ContentBlockConfig[]}) {
-    return <>
-        {data.map((block) => <ContentBlock key={block.id} data={block} />)}
-    </>;
+  return <>
+      {data.map((block) => <ContentBlock key={block.id} data={block} />)}
+  </>;
 }
 
 // eslint-disable-next-line complexity
 export function ContentBlock({data}: {data: ContentBlockConfig}) {
-    switch (data.type) {
-        case 'hero':
-            return <HeroBlock data={data} />;
-        case 'html':
-            return <HTMLBlock data={data} />;
-        case 'cta_block':
-            return <CTABlock data={data} />;
-        case 'divider':
-            return <DividerBlock data={data} />;
-        case 'text':
-            return <RichTextBlock data={data} />;
-        case 'section':
-            return <SectionBlock data={data} />;
-        case 'cards_block':
-           return <CardsBlock data={data} />;
-        case 'links_group':
-           return <LinksBlock data={data} />;
-        case 'quote':
-           return <QuoteBlock data={data} />;
-        default:
-            return <pre>{JSON.stringify(data, null, 2)}</pre>;
-    }
+  const Block = React.useContext(BlockContext)[data.type];
+  if (!Block) return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return <Block data={data} />;
 }
