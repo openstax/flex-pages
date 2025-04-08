b07a9fe435e610d189a38506954ef197cf822c80
tom <tom.g.woodward@gmail.com>
2025-04-08 07:33:00 -0400

    bump versions; cards have rich text

--- packages/flex-page-renderer/src/blocks/CardsBlock.tsx
+++ packages/flex-page-renderer/src/blocks/CardsBlock.tsx
@@ -35,7 +35,7 @@ CardsBlock.blockConfig = {
   categories: ['content'],
   fields: [
     {name: 'cards', label: 'Cards', type: 'list', fields: [
-      {name: 'text', label: 'Card Text', type: 'text', required: true},
+      {name: 'rich-text', label: 'Card Text', type: 'text', required: true},
       {name: 'ctaBlock', label: 'Call To Action', type: 'list', fields: ctaLinkFieldConfig, max: 1},
     ]},
     {name: 'config', label: 'Config', type: 'configs', configs: [
