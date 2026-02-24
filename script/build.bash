234d5d3daf91c23e01d97e93fafe35f870c90822
tom <tom.g.woodward@gmail.com>
2026-02-24 11:06:29 -0500

    rip out styled-components

--- script/build.bash
+++ script/build.bash
@@ -11,4 +11,6 @@ tsc_args=(--noEmit false --declaration)
 mkdir -p dist
 
 yarn -s tsc --project tsconfig.without-specs.esm.json "${tsc_args[@]}"
+rsync -av --include="*.css" --include="*/" --exclude="*" src/ dist/esm/
 yarn -s tsc --project tsconfig.without-specs.cjs.json "${tsc_args[@]}"
+rsync -av --include="*.css" --include="*/" --exclude="*" src/ dist/cjs/
--- src/CollapsibleFieldset.tsx
+++ src/CollapsibleFieldset.tsx
@@ -1,30 +1,11 @@
-import React from 'react';
 import cn from 'classnames';
-import styled from 'styled-components';
-
-const Fieldset = styled.fieldset`
-  &.collapsed > *:not(legend) {
-    display: none;
-  }
-
-  > legend, > legend button {
-    height: 3rem;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-  }
-
-  > legend button {
-    background: none;
-    border: none;
-  }
-`;
+import React from 'react';
+import './CollapsibleFieldset.css';
 
 export const CollapsibleFieldset = ({children, ...props}: React.PropsWithChildren<{legend: string}>) => {
   const [collapsed, setCollapsed] = React.useState(false);
 
-  return <Fieldset className={cn({collapsed})}>
+  return <fieldset className={cn('collapsible-fieldset', {collapsed})}>
     <legend>
       <button type="button"
         onClick={() => setCollapsed(previous => !previous)}
@@ -32,7 +13,7 @@ export const CollapsibleFieldset = ({children, ...props}: React.PropsWithChildre
       {props.legend}
     </legend>
     {children}
-  </Fieldset>
+  </fieldset>;
 };
 
 const foldIcon = <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m356-160-56-56 180-180 180 180-56 56-124-124-124 124Zm124-404L300-744l56-56 124 124 124-124 56 56-180 180Z"/></svg>;
