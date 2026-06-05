b19414fc38238f25ef631731d191dcd42141ae5d
tom <tom.g.woodward@gmail.com>
2026-06-05 14:36:02 -0400

    adding dist files

---
+++ dist/cjs/blocks/QuoteBlock.component.d.ts
@@ -0,0 +1,21 @@
+import { ImageFields } from '../components/Image.js';
+import './QuoteBlock.css';
+type QuoteConfig = {
+    type: 'accent_color';
+    value: string;
+};
+export interface QuoteBlockConfig {
+    id: string;
+    type: 'quote';
+    value: {
+        image: ImageFields;
+        content: string;
+        name: string;
+        title?: string;
+        config: QuoteConfig[];
+    };
+}
+export declare function QuoteBlock({ data }: {
+    data: QuoteBlockConfig;
+}): import("react/jsx-runtime").JSX.Element;
+export {};
---
+++ dist/cjs/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.resolveContentBlocks = void 0;
+var resolveContentBlocks_js_1 = require("./resolveContentBlocks.js");
+Object.defineProperty(exports, "resolveContentBlocks", { enumerable: true, get: function () { return resolveContentBlocks_js_1.resolveContentBlocks; } });
